9c8ff832f5e6e7e29475c7c0a9b324a494e0dc0f
mrtnu <mrtn.usym@gmail.com>
2026-08-29 08:43:59 +0900

    Prototype formula-to-pattern transitions

--- scripts/core.cjs
+++ scripts/core.cjs
@@ -5,7 +5,7 @@ function loadCore(html=fs.readFileSync(path.join(root,'index.html'),'utf8')){
   const end=script.indexOf(';',script.indexOf('const ORDER='))+1;
   if(!end)throw new Error('Missing structure registry');
   const context=vm.createContext({});
-  vm.runInContext(script.slice(0,end)+'\nthis.core={S,ORDER,svgFromDots,brandLogoSVG,PALETTES,normalizeParams:typeof normalizeParams==="function"?normalizeParams:null};',context);
+  vm.runInContext(script.slice(0,end)+'\nthis.core={S,ORDER,svgFromDots,brandLogoSVG,PALETTES,normalizeParams:typeof normalizeParams==="function"?normalizeParams:null,FORMULAS,formulaFor};',context);
   return context.core;
 }
 function defaults(S,id){return Object.fromEntries(S[id].params.map(p=>[p.k,p.d]));}
